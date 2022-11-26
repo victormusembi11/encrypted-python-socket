477940f0e12eafc4ac3e8db81ccf727e6fd12a70
victormusembi11 <victor.musembi11@gmail.com>
2022-11-26 20:20:04 +0300

    socket server function

--- server.py
+++ server.py
@@ -11,29 +11,30 @@ args = parser.parse_args()
 pub_key, priv_key = rsa.newkeys(1024)
 conn_pub_key = None
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((args.host, args.port))
-s.listen()
-print('listening for incoming connections...')
+
+def socket_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((args.host, args.port))
+    s.listen()
+    print('listening for incoming connections...')
+    return s
 
 
 def accept_connection(s: socket.socket):
     global conn_pub_key
 
-    conn, addr = s.accept()
+    conn, _ = s.accept()
     print('connection to client established...')
 
     # Exchange public keys with connected client
     conn.send(bytes(pub_key.save_pkcs1('PEM')))
     conn_pub_key = rsa.PublicKey.load_pkcs1(conn.recv(1024))
-
     return conn
 
 
 def send_msg(conn: socket.socket):
     while True:
         msg = input('> ')
-
         encrypt_msg = rsa.encrypt(msg.encode(), conn_pub_key)
         conn.send(encrypt_msg)
         print(f'sent: {msg}\n', end='')
@@ -46,6 +47,7 @@ def recv_msg(conn: socket.socket):
         print(f'recv: {decrypt_msg}\n> ', end='')
 
 
+s = socket_server()
 conn = accept_connection(s)
 
 threading.Thread(target=send_msg, args=(conn, )).start()
